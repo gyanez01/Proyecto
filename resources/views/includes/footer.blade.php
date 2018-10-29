c0daab14e622e5d0343b45210655d9f616499e4f
gyanez01 <gyanez394@puce.edu.ec>
2018-10-28 21:05:06 -0500

    commit 2

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,18 @@
+<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #333;
+    color: white;
+    text-align: center;
+}
+</style>
+
+<div class="footer">
+<div id="copyright text-right">© Copyright 2018 LTIC</div>
+</div>
+
+
+
